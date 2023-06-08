f441c92edd42be3f7d3a7b4c8cde3585576f4fd1
vidovichb <vidovich.blaz@gmail.com>
2023-06-08 17:30:56 +0200

    benchmarks

--- app-languages/java/src/main/java/com/example/reactiveapijava/routers/OrderRouter.java
+++ app-languages/java/src/main/java/com/example/reactiveapijava/routers/OrderRouter.java
@@ -3,13 +3,11 @@ package com.example.reactiveapijava.routers;
 import com.example.reactiveapijava.services.OrderService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 @Configuration(proxyBeanMethods = false)
 public class OrderRouter {
